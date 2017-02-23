240e31261fc18235b9f97fd7a8eda5e5fff93a84
hristoterezov <hristo@jitsi.org>
2017-02-23 17:18:56 -0600

    ref(participants): Move Avatar and ParticipantView to base/participants

--- react/features/base/participants/index.js
+++ react/features/base/participants/index.js
@@ -2,6 +2,7 @@ export * from './actions';
 export * from './actionTypes';
 export * from './constants';
 export * from './functions';
+export * from './components';
 
 import './middleware';
 import './reducer';
